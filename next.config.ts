29244004fa4f34979b9b8df6896df5d227452c3b
Alan Almaguer <alalmag2@gmail.com>
2025-01-13 16:42:37 -0600

    se añade google drive para cargar imagenes

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'cdn.pixabay.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        
+      },
     ],
   },
 };
